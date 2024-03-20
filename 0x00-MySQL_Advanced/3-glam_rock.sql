4c699cb368565d41c69dc852e60eaab4a7127f9d
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-20 21:46:04 +0000

    kfjsdl

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,5 @@
 -- "glam rock" bands
-SELECT
-    band_name,
-    IFNULL(split - formed, YEAR(2022) - formed) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
