d07323139706e223ac2deca4ccabf45b9a07beee
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-20 21:53:27 +0000

    valid emal

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,10 +2,10 @@
 DELIMITER $$
 
 CREATE TRIGGER reset_valid_email
-BEFORE UPDATE ON your_table_name
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email != OLD.email THEN
+    IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END$$
