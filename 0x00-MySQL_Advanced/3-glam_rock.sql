2b18685e40949e3cd0c12a0c97f7923331719a65
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-20 21:44:27 +0000

    kfjds

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- "glam rock" bands
 SELECT
     band_name,
-    IFNULL(split, 2022) - INFULL(formed, 0) AS lifespan
+    IFNULL(split - formed, YEAR(2022) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
