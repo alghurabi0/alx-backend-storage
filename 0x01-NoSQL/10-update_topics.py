2bcbdc83d9e51809a21081cb5502f21f08e69ecc
alghurabi01 <zaid.alghurabi01@gmail.com>
2024-03-25 11:22:16 -0700

    kdfjs

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+""" change value of a doc or docs """
 
 
 def update_topics(mongo_collection, name, topics):
+    """ change value of a doc of docs """
     q = {"name": name}
     n = {$set: {"topics": topics}}
     mongo_collection.update_many(q, n)
